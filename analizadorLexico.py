bb6ab4239cea97506b25abfc8c1fedc3ad235c72
solinaldevelop <developer@solinal.org>
2024-06-15 23:16:16 -0500

    Tokens

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -1,4 +1,4 @@
-from ply.lex import lex
+import ply.lex as lex
 
 reserved = {"as": "AS",
             "async" : "ASYNC",
@@ -40,10 +40,59 @@ reserved = {"as": "AS",
              }
 
 tokens = (
-    "INTEGER",
     "FLOAT",
+    "INTEGER",
     "CHAR",
     "BOOL",
     "CPOINTER",
     "REFERENCE",
 )+tuple(reserved.values())
+
+#Definicion token INTEGER John Cañarte
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_VARIABLE(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value,'VARIABLE')
+    return t 
+
+def t_FLOAT(t):
+    r'[-]?[0-9]*\.[0-9]*'
+    t.value = float(t.value)
+    return t
+
+lexer = lex.lex()
+
+data = "12"
+
+lexer.input(data)
+
+
+
+#Reglas generales
+# Saltos de linea
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# Ignorar espacios y TAB
+
+t_ignore  = ' \t'
+
+# Errores
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Tokenize
+
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break    
+    print(tok)
