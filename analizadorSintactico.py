ce11d021ab1e4c532a804f204920b383f1775cb7
jecanart <jecanart@espol.edu.ec>
2024-07-07 19:33:06 -0500

    merge

--- analizadorSintactico.py
+++ analizadorSintactico.py
@@ -119,7 +119,9 @@ def p_compOperator(p):
 def p_value(p):
     '''value : VARIABLE
              | INTEGER
-             | FLOAT'''
+             | FLOAT
+             | STRING
+             | BOLL'''
     if isinstance(p[1], str) and p[1] in variables:
         p[0] = variables[p[1]]
     else:
