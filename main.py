089e7edeaacf56e2f5729c3f01e4ce478bd84187
jecanart <jecanart@espol.edu.ec>
2024-07-11 00:08:01 -0500

    Update GUI

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import tkinter as tk
 from tkinter import scrolledtext
 from analizadorLexico import lexer, lexical_errors
-from analizadorSintactico import parser, syntactic_errors
+from analizadorSintactico import parser, syntactic_errors, variables, mutables
 
 def analyze_code():
     code = code_input.get("1.0", "end-1c")
     
+    # Reiniciar las variables
+    variables.clear()
+    mutables.clear()
+
     # Reiniciar los errores
     lexical_errors.clear()
     syntactic_errors.clear()
     
     # Análisis léxico
     lexer.input(code)
-    for token in lexer:
-        pass  # Proceso de tokenización
+
+    #Mostrar Tokens
+    error_output.delete("1.0", "end")
+    error_output.insert("end", f"\n")
+    error_output.insert("end", f"Tokens encontrados:\n")
+    error_output.insert("end", f"\n")
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        error_output.insert("end", f"Token('{tok.type}', '{tok.value}')\n")
 
     # Análisis sintáctico
     parser.parse(code)
     
     # Mostrar errores
-    error_output.delete("1.0", "end")
     if lexical_errors or syntactic_errors:
         if lexical_errors:
             error_output.insert("1.0", "Errores Léxicos:\n" + "\n".join(lexical_errors) + "\n")
         if syntactic_errors:
-            error_output.insert("1.0", "Errores Sintácticos:\n" + "\n".join(syntactic_errors))
+            error_output.insert("1.0", "Errores Sintácticos:\n" + "\n".join(syntactic_errors)+ "\n")
     else:
-        error_output.insert("1.0", "No se han detectado problemas")
+        error_output.insert("1.0", "No se han detectado problemas\n")
 
 root = tk.Tk()
 root.title("Análisis léxico, semántico y sintáctico de código en Rust")
@@ -43,7 +56,7 @@ code_input.pack()
 analyze_button = tk.Button(frame, text="Analizar", command=analyze_code)
 analyze_button.pack(pady=10)
 
-error_label = tk.Label(frame, text="Errores:")
+error_label = tk.Label(frame, text="Análisis:")
 error_label.pack(anchor='w')
 
 error_output = scrolledtext.ScrolledText(frame, height=10, width=80)
